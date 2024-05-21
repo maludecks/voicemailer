9f2f8caf36fbd98ff96922c31772189e4336a9fa
Malu Decks <m.decks@hotmail.com>
2024-05-21 12:48:01 +0200

    Init amplify

---
+++ amplify/backend.ts
@@ -0,0 +1,9 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+defineBackend({
+  auth,
+});
